b473d6c9675288c8ca1b56a7d081e4eef3ee171c
bayley105 <79952159+bayley105@users.noreply.github.com>
2021-07-10 02:03:51 +1000

    set up basics

---
+++ Assets/Scripts/playerMovement_script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement_script : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    Vector2 velocity;
+
+    public float runSpeed;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        body.velocity = velocity;
+    }
+}
